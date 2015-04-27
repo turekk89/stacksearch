5445183d483b10d7664e638f8a7f6a7a8d207b90
Krzysztof Turek <k.turek89@gmail.com>
2015-04-27 13:51:15 +0200

    [SS] Add Toolbar + styling

--- app/src/main/java/pl/turek/stacksearch/ui/BaseContentActivity.java
+++ app/src/main/java/pl/turek/stacksearch/ui/BaseContentActivity.java
@@ -1,8 +1,13 @@
 package pl.turek.stacksearch.ui;
 
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import pl.turek.stacksearch.R;
 
 /**
@@ -12,10 +17,20 @@ public abstract class BaseContentActivity extends BaseFragmentActivity {
 
     private static final int FRAGMENT_CONTAINER_ID = R.id.base_content_activity_fragment_container;
 
+    @InjectView(R.id.toolbar)
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.base_content_activity);
+
+        ButterKnife.inject(this);
+
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+            ViewCompat.setElevation(mToolbar, 0); // remove shadow for API 21 and higher
+        }
 
         createFragmentIfNeeded(savedInstanceState);
     }

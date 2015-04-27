788e6cd24546bfa385bfaf1f8f0158a8f2e393ab
Krzysztof Turek <k.turek89@gmail.com>
2015-04-27 09:20:34 +0200

    [SS] Add empty SearchFragment

---
+++ app/src/main/java/pl/turek/stacksearch/search/SearchFragment.java
@@ -0,0 +1,9 @@
+package pl.turek.stacksearch.search;
+
+import pl.turek.stacksearch.ui.BaseFragment;
+
+/**
+ * @author Krzysztof Turek (2015-04-27).
+ */
+public class SearchFragment extends BaseFragment {
+}
